fecf8ae0f11f6ffdac4e2d031ad543c4b3023e5f
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-10 17:02:43 +0300

    import HttpClientModule and FormsModule to root file as well as provide the Usersearch service to the entire application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersearchService } from './services/usersearch.service';
 import { UserComponent } from './components/user/user.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchuserComponent } from './components/searchuser/searchuser.component';
@@ -16,9 +19,11 @@ import { SearchuserComponent } from './components/searchuser/searchuser.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UsersearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
